0df249069c5fac573dff95121ed973accdad1107
caarlosdamian <dapecarlos@gmail.com>
2022-06-20 15:05:09 -0300

    added grid and some styling

---
+++ src/sections/middle/Middle.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Middle.scss";
+
+export const Middle = () => {
+  return (
+    <div className="middle-grid">
+      <div className="middle__grid-card">
+        <h1 className="middle-grid-header">Tranform you Brand</h1>
+        <span className="middle-grid__subtitle">
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </span>
+        <span className="middle-grid__subtext">Learn more</span>
+      </div>
+      <div className="middle__grid-card">
+        <h1 className="middle-grid-header">Tranform you Brand</h1>
+        <span className="middle-grid__subtitle">
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </span>
+        <span className="middle-grid__subtext">Learn more</span>
+      </div>
+      <div className="middle__grid-card">
+        <h1 className="middle-grid-header">Tranform you Brand</h1>
+        <span className="middle-grid__subtitle">
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </span>
+        <span className="middle-grid__subtext">Learn more</span>
+      </div>
+      <div className="middle__grid-card">
+        <h1 className="middle-grid-header">Tranform you Brand</h1>
+        <span className="middle-grid__subtitle">
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </span>
+        <span className="middle-grid__subtext">Learn more</span>
+      </div>
+    </div>
+  );
+};
