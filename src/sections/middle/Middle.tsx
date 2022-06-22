f2a5922f79ce9fa3f2fa72aab781723a6f82c1f8
caarlosdamian <dapecarlos@gmail.com>
2022-06-21 19:52:18 -0500

    fix styling;

--- src/sections/middle/Middle.tsx
+++ src/sections/middle/Middle.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import egg from "../../images/desktop/image-transform.jpg";
 import "./Middle.scss";
 
 export const Middle = () => {
@@ -11,16 +12,13 @@ export const Middle = () => {
           grow fast. Engage your clients through compelling visuals that do most
           of the marketing for you.
         </span>
-        <span className="middle-grid__subtext">Learn more</span>
+        <div className="bottom__button">
+          <span className="middle-grid__subtext">Learn more</span>
+          <div className="line"></div>
+        </div>
       </div>
-      <div className="middle__grid-card">
-        <h1 className="middle-grid-header">Tranform you Brand</h1>
-        <span className="middle-grid__subtitle">
-          We are a full-service creative agency specializing in helping brands
-          grow fast. Engage your clients through compelling visuals that do most
-          of the marketing for you.
-        </span>
-        <span className="middle-grid__subtext">Learn more</span>
+      <div className="middle__grid-card-img">
+        <img src={egg} alt="egg" />
       </div>
       <div className="middle__grid-card">
         <h1 className="middle-grid-header">Tranform you Brand</h1>
